c2bda5788b49043340e7622d52bd26d37774eef1
ramanuj2 <ramanuj@fplabs.tech>
2022-03-23 15:26:59 +0530

    websocket with java server

---
+++ src/WebSokcet.java
@@ -0,0 +1,20 @@
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.security.NoSuchAlgorithmException;
+
+public class WebSokcet {
+
+    public static void main(String[] args) throws IOException, NoSuchAlgorithmException {
+        ServerSocket serverSocket = new ServerSocket(80);
+        try{
+            System.out.println("server started at lovalhost at portt 80");
+
+            Socket client = serverSocket.accept();
+            System.out.println(client.getInetAddress() + " connected");
+
+        }catch (IOException e)
+        {}
+
+    }
+}
